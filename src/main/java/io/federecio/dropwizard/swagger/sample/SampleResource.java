2368dc533e2b8109a3b38a45fce0843705237983
Federico Recio <federecio@gmail.com>
2014-09-05 10:56:08 -0700

    add a few more sample resource methods

--- src/main/java/io/federecio/dropwizard/swagger/sample/SampleResource.java
+++ src/main/java/io/federecio/dropwizard/swagger/sample/SampleResource.java
@@ -5,6 +5,8 @@ import com.wordnik.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -18,5 +20,19 @@ public class SampleResource {
     @ApiOperation("Sample endpoint")
     public Response get() {
         return Response.ok(new SamplePojo("Federico", 1234)).build();
+    }
+
+    @GET
+    @ApiOperation("Sample endpoint with path param")
+    @Path("/hello-with-path-param/{name}")
+    public Response getWithPathParam(@PathParam("name") String name) {
+        return Response.ok(new SamplePojo("Hello " + name, 333)).build();
+    }
+
+    @GET
+    @ApiOperation("Sample endpoint with query param")
+    @Path("/hello-with-query-param")
+    public Response getWithQueryParam(@QueryParam("name") String name) {
+        return Response.ok(new SamplePojo("Hello " + name, 444)).build();
     }
 }
